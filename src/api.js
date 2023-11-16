14f6be2ddecd212016788c589c71c34f3c0b9048
Sunny Patel <sunnypatel2048@gmail.com>
2023-11-16 21:16:47 +0530

    Built current weather component

--- src/api.js
+++ src/api.js
@@ -1,9 +1,12 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'RAPID_API_KEY',
+		'X-RapidAPI-Key': '57c4881d11msh14fc0c693f1f271p1454f5jsn47d689d4bf2b',
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '1cd67c6f7bd68b1764eba7d59fd4cd03';
